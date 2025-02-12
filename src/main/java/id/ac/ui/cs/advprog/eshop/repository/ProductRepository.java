4d6459f0830cff886098a4e1ac70f21fb43c09da
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-02-12 12:11:59 +0700

    update edit-product feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -31,14 +31,9 @@ public class ProductRepository {
         return null;
     }
 
-    public Product update(Product product) {
-        for (Product existProduct : productData) {
-            if (existProduct.getProductId().equals(product.getProductId())) {
-                existProduct.setProductName(product.getProductName());
-                existProduct.setProductQuantity(product.getProductQuantity());
-                break;
-            }
-        }
-        return product;
+    public void update(String productId, Product updatedProduct) {
+        Product product = getId(productId);
+        product.setProductName(updatedProduct.getProductName());
+        product.setProductQuantity(updatedProduct.getProductQuantity());
     }
 }
