1b45f2c636db8bbf5340d63708c06c7624995ace
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-02-16 15:49:10 +0700

    fix issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
 	jacoco
-	id("org.sonarqube") version "6.0.1.5171"
+	id("org.sonarqube") version "4.4.1.3373"
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 }
