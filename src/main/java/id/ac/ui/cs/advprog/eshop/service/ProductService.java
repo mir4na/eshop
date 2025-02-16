fade7b478c3a254982a75bff2e865ee9f6387add
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-02-16 11:52:24 +0700

    remove public modifier

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public Product getId(String productId);
-    public void update(String productId, Product updatedProduct);
-    public void delete(String productId);
-    public List<Product> findAll();
+    Product create(Product product);
+    Product getId(String productId);
+    void update(String productId, Product updatedProduct);
+    void delete(String productId);
+    List<Product> findAll();
 }
