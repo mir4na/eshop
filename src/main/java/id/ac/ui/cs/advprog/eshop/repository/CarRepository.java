43075806def3729b924b02eeaf1464b5106d03cf
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-02-21 21:03:25 +0700

    change name from createCar to create

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -13,7 +13,7 @@ public class CarRepository {
 
     private List<Car> carData = new ArrayList<>();
 
-    public Car createCar(Car car) {
+    public Car create(Car car) {
         if(car.getCarId() == null) {
             UUID uuid = UUID.randomUUID();
             car.setCarId(uuid.toString());
