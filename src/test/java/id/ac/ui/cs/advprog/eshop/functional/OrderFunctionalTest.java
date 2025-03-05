862a18fbc8102725aab1be56458e3b8baadc74cb
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2025-03-05 11:54:25 +0700

    [GREEN] add controller for order

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/OrderFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/OrderFunctionalTest.java
@@ -1,120 +1,137 @@
 package id.ac.ui.cs.advprog.eshop.functional;
 
-import id.ac.ui.cs.advprog.eshop.model.Order;
-import id.ac.ui.cs.advprog.eshop.model.Product;
-import id.ac.ui.cs.advprog.eshop.service.OrderService;
-import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import io.github.bonigarcia.seljup.SeleniumJupiter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class OrderFunctionalTest {
+@DirtiesContext
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class OrderFunctionalTest {
+    @LocalServerPort
+    private int serverPort;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
 
-    @Autowired
-    private ProductService productService;
-
-    @Autowired
-    private OrderService orderService;
-
-    private Product testProduct;
-    private Order testOrder;
+    String baseUrl;
+    private String createOrderUrl;
+    private String orderHistoryUrl;
+    private String createProductUrl;
 
     @BeforeEach
-    public void setup() {
-        testProduct = new Product();
-        testProduct.setProductName("Test Product");
-        testProduct.setProductQuantity(10);
-        testProduct = productService.create(testProduct);
-
-        List<Product> products = new ArrayList<>();
-        products.add(testProduct);
-        testOrder = new Order(
-                "test-order-id",
-                products,
-                System.currentTimeMillis(),
-                "testUser"
-        );
-        testOrder = orderService.createOrder(testOrder);
+    void setUpTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+        createOrderUrl = String.format("%s/order/create", baseUrl);
+        orderHistoryUrl = String.format("%s/order/history", baseUrl);
+        createProductUrl = String.format("%s/product/create", baseUrl);
     }
 
     @Test
-    public void testCreateOrderPage() throws Exception {
-        mockMvc.perform(get("/order/create"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("order/create-order"))
-                .andExpect(model().attributeExists("products"));
+    void createOrderPage_titleIsCorrect(ChromeDriver driver) {
+        driver.get(createOrderUrl);
+        String pageTitle = driver.getTitle();
+        assertEquals("Create Order", pageTitle);
     }
 
     @Test
-    public void testCreateOrderSubmission() throws Exception {
-        mockMvc.perform(post("/order/create")
-                        .param("author", "testUser")
-                        .param("selectedProducts", testProduct.getProductId()))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("/order/pay/*"));
+    void createOrderPage_hasCorrectElements(ChromeDriver driver) {
+        driver.get(createOrderUrl);
+
+        WebElement authorInput = driver.findElement(By.name("author"));
+        assertNotNull(authorInput);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        assertNotNull(submitButton);
     }
 
     @Test
-    public void testCreateOrderWithNoProducts() throws Exception {
-        mockMvc.perform(post("/order/create")
-                        .param("author", "testUser"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/order/create?error=noProductsSelected"));
+    void orderHistoryPage_hasCorrectElements(ChromeDriver driver) {
+        driver.get(orderHistoryUrl);
+
+        String pageTitle = driver.getTitle();
+        assertEquals("Order History", pageTitle);
+
+        WebElement authorInput = driver.findElement(By.name("author"));
+        assertNotNull(authorInput);
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        assertNotNull(submitButton);
     }
 
     @Test
-    public void testOrderHistoryForm() throws Exception {
-        mockMvc.perform(get("/order/history"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("order/history-form"));
+    void createOrder_withValidData(ChromeDriver driver) {
+        boolean stayInPage = true;
+
+        driver.get(createOrderUrl);
+
+        WebElement authorInput = driver.findElement(By.name("author"));
+        authorInput.sendKeys("testUser");
+
+        List<WebElement> productCheckboxes = driver.findElements(By.cssSelector("input[type='checkbox'][name='selectedProducts']"));
+        if (!productCheckboxes.isEmpty()) {
+            productCheckboxes.getFirst().click();
+            stayInPage = false;
+        }
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        if (!stayInPage) {
+            assertTrue(driver.getCurrentUrl().contains("/order/pay"), "Should redirect to order payment page");
+        } else {
+            assertTrue(driver.getCurrentUrl().contains("/order/create"), "Should stay in the same page");
+        }
+
     }
 
     @Test
-    public void testOrderHistorySubmission() throws Exception {
-        mockMvc.perform(post("/order/history")
-                        .param("author", "testUser"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("order/history"))
-                .andExpect(model().attributeExists("orders"))
-                .andExpect(model().attribute("orders", not(empty())));
-    }
+    void orderHistory_withValidAuthor(ChromeDriver driver) {
+        driver.get(createProductUrl);
+        String expectedProductName = "Hatsune Miku";
+        int expectedProductQuantity = 150;
 
-    @Test
-    public void testOrderPaymentPage() throws Exception {
-        mockMvc.perform(get("/order/pay/" + testOrder.getId()))
-                .andExpect(status().isOk())
-                .andExpect(view().name("order/pay"))
-                .andExpect(model().attributeExists("order"));
-    }
+        WebElement nameInputField = driver.findElement(By.id("nameInput"));
+        nameInputField.sendKeys(expectedProductName);
 
-    @Test
-    public void testOrderPaymentSubmission() throws Exception {
-        mockMvc.perform(post("/order/pay/" + testOrder.getId())
-                        .param("paymentMethod", "VOUCHER"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("order/payment-success"))
-                .andExpect(model().attributeExists("paymentId"));
-    }
+        WebElement quantityInputField = driver.findElement(By.id("quantityInput"));
+        quantityInputField.sendKeys(String.valueOf(expectedProductQuantity));
 
-    @Test
-    public void testOrderPaymentWithInvalidOrder() throws Exception {
-        mockMvc.perform(get("/order/pay/non-existent-order"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/order/history?error=orderNotFound"));
+        WebElement submitButtonProduct = driver.findElement(By.tagName("button"));
+        submitButtonProduct.click();
+
+        driver.get(createOrderUrl);
+        WebElement authorInput = driver.findElement(By.name("author"));
+        authorInput.sendKeys("testUser");
+        List<WebElement> productCheckboxes = driver.findElements(By.cssSelector("input[type='checkbox'][name='selectedProducts']"));
+        if (!productCheckboxes.isEmpty()) {
+            productCheckboxes.getFirst().click();
+        }
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        driver.get(orderHistoryUrl);
+
+        WebElement historyAuthorInput = driver.findElement(By.name("author"));
+        historyAuthorInput.sendKeys("testUser");
+
+        WebElement historySubmitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        historySubmitButton.click();
+
+        List<WebElement> orderRows = driver.findElements(By.cssSelector("table tbody tr"));
+        assertFalse(orderRows.isEmpty(), "Order history should display orders");
     }
 }
